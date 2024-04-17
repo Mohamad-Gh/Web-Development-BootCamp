402b5670d75c95521e97e8cc1f3d56d1266be2ca
Mohamad GH <mohamad.ghalebizade@gmail.com>
2024-04-17 12:37:20 -0700

    complete part 28

--- 36 - React.js/28-React-Conditional-Rendering/src/components/App.jsx
+++ 36 - React.js/28-React-Conditional-Rendering/src/components/App.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Login from "./Login";
 
-isLoggedIn = false;
+const isLoggedIn = false;
 
-function renderCondition() {
-  if (isLoggedIn) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
+// function renderCondition() {
+//   if (isLoggedIn) {
+//     return <h1>Hello</h1>;
+//   } else {
+//     return <Login />;
+//   }
+// }
 
 function App() {
-  return <div className="container">{renderCondition()}</div>;
+  return (
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+  );
 }
 
 export default App;
--- 36 - React.js/28-React-Conditional-Rendering/src/components/Login.jsx
+++ 36 - React.js/28-React-Conditional-Rendering/src/components/Login.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Input from "./Input";
 
-function Login(props) {
-  <form className="form">
-    <Input type="text" placeholder="Username" />
-    <Input type="password" placeholder="Password" />
-    <button type="submit">Login</button>
-  </form>;
+function Login() {
+  return (
+    <form className="form">
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
 }
 
 export default Login;

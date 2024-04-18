0201628dd37e9e609d6068492b4eb67f2afb7834
Mohamad GH <mohamad.ghalebizade@gmail.com>
2024-04-17 22:50:59 -0700

    Complete part 30

--- 36 - React.js/30-State-In-React-Demo/src/components/App.jsx
+++ 36 - React.js/30-State-In-React-Demo/src/components/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
+let isDone = false;
+
+const strikeThrough = { textDecoration: "line-through" };
+
+function strike() {
+  // isDone = true;
+  document.getElementById("root").style.textDecoration = "line-through";
+}
+function unStrike() {
+  // isDone = false;
+  document.getElementById("root").style.textDecoration = "unset";
+}
+
 function App() {
   return (
     <div>
-      <p>Buy milk</p>
+      <p style={isDone ? strikeThrough : null}>Buy milk</p>
+      <button onClick={strike}>Change to Strike Through</button>
+      <button onClick={unStrike}>Change Back</button>
     </div>
   );
 }

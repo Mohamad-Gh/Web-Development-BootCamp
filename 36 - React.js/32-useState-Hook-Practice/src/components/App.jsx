e7e35690af0d9f1ee85781d2509b86ec6fcc8e94
Mohamad GH <mohamad.ghalebizade@gmail.com>
2024-04-18 21:25:52 -0700

    Complete Phase 1 of part 32

--- 36 - React.js/32-useState-Hook-Practice/src/components/App.jsx
+++ 36 - React.js/32-useState-Hook-Practice/src/components/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+
+  const [timer, setTime] = useState(time);
+
+  function settingTime() {
+    let time = new Date().toLocaleTimeString();
+    setTime(time);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{timer}</h1>
+      {/* <button onClick={setting}>Get Time</button> */}
+      <button onClick={settingTime}>Get Time</button>
     </div>
   );
 }

35e25a5b6b9c6b498852b6b40da524844daa8606
Mohamad GH <mohamad.ghalebizade@gmail.com>
2024-04-18 20:37:36 -0700

    add App

--- 36 - React.js/31-React-Hooks-useState/src/components/App.jsx
+++ 36 - React.js/31-React-Hooks-useState/src/components/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const initialValue = 0;
+  const [count, setCount] = useState(initialValue);
+
+  function increment() {
+    setCount(count + 1);
+  }
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+    </div>
+  );
 }
 
 export default App;

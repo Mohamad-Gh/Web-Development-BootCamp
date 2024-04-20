f584f66eb49052b8b8c6d05bb450f80b7016e68b
Mohamad GH <mohamad.ghalebizade@gmail.com>
2024-04-19 19:39:53 -0700

    Complete Part 36 using onSubmit and Form

--- 36 - React.js/36-React-Forms/src/components/App.jsx
+++ 36 - React.js/36-React-Forms/src/components/App.jsx
@@ -3,26 +3,31 @@ import React, { useState } from "react";
 function App() {
   const [name, setName] = useState("");
   const [userName, setUserName] = useState("");
-  let newName = "";
+
   function handleChange(event) {
     setName(event.target.value);
-    // console.log(name);
   }
   function handleSubmit() {
     setUserName(name);
   }
 
+  // function handleSubmit() {
+  //   setUserName(name);
+  // }
+
   return (
-    <div className="container">
-      <h1>Hello {userName}</h1>
-      <input
-        onChange={handleChange}
-        type="text"
-        placeholder="What's your name?"
-        value={name}
-      />
-      <button onClick={handleSubmit}>Submit</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="container">
+        <h1>Hello {userName}</h1>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   );
 }
 

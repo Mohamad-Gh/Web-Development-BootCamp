c125edec68e919c2c8f6827815b1bb7c843f2cd1
Mohamad GH <mohamad.ghalebizade@gmail.com>
2024-04-20 09:48:40 -0700

    Add FName and LName handler functions

--- 36 - React.js/38-Changing-Complex-State/src/components/App.jsx
+++ 36 - React.js/38-Changing-Complex-State/src/components/App.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+
+  function handleFName(event) {
+    setFName(event.target.value);
+  }
+
+  function handleLName(event) {
+    setLName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fName} {lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input
+          onChange={handleFName}
+          name="fName"
+          placeholder="First Name"
+          value={fName}
+        />
+        <input
+          onChange={handleLName}
+          name="lName"
+          placeholder="Last Name"
+          value={lName}
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );

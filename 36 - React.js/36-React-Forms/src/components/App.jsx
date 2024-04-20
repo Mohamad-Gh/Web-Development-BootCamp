335a33dd918e5e33d055b85ea0382b1f498c9edb
Mohamad GH <mohamad.ghalebizade@gmail.com>
2024-04-19 18:35:15 -0700

    First Solution

--- 36 - React.js/36-React-Forms/src/components/App.jsx
+++ 36 - React.js/36-React-Forms/src/components/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  let newName = "";
+  function handleChange(event) {
+    setName(event.target.value);
+    // console.log(name);
+  }
+  function handleSubmit() {
+    setUserName(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {userName}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }

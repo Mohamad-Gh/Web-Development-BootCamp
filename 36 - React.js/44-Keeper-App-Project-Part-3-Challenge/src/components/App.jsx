185e01c0e1ac0d8cc21c7576f0af23f3d80089db
Mohamad GH <mohamad.ghalebizade@gmail.com>
2024-04-21 14:11:32 -0700

    Fix passing title and content to the Parent Component, prevent default form behavior, Rest the title anc content after adding

--- 36 - React.js/44-Keeper-App-Project-Part-3-Challenge/src/components/App.jsx
+++ 36 - React.js/44-Keeper-App-Project-Part-3-Challenge/src/components/App.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [box, setBox] = useState([{ boxTitle: "", boxText: "" }]);
+
+  function handleAdd(obj) {
+    setBox((prvs) => [obj, ...prvs]);
+    console.log(typeof box);
+  }
+
   return (
     <div>
       <Header title="Keeper" />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={handleAdd} />
+      {box.map((element) => (
+        <Note
+          id={element.boxTitle}
+          key={element.boxTitle}
+          title={element.boxTitle}
+          content={element.boxText}
+        />
+      ))}
       <Footer author="MGH" />
     </div>
   );
--- 36 - React.js/44-Keeper-App-Project-Part-3-Challenge/src/components/CreateArea.jsx
+++ 36 - React.js/44-Keeper-App-Project-Part-3-Challenge/src/components/CreateArea.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  function handleChangeTitle(event) {
+    setTitle(event.target.value);
+  }
+
+  function handleChangeText(event) {
+    setText(event.target.value);
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form
+        onSubmit={(event) => {
+          props.onAdd({ boxTitle: title, boxText: text });
+          event.preventDefault();
+          setText("");
+          setTitle("");
+        }}
+      >
+        <input
+          onChange={handleChangeTitle}
+          name="title"
+          placeholder="Title"
+          value={title}
+        />
+        <textarea
+          onChange={handleChangeText}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={text}
+        />
+        <button
+        // onClick={() => {
+        // props.onAdd({ boxTitle: title, boxText: text });
+        // setTitle("");
+        // setText("");
+        // }}
+        >
+          Add
+        </button>
       </form>
     </div>
   );

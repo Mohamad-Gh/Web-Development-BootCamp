bfa34edc7281755db56b287d7828875cee75d0a1
Mohamad GH <mohamad.ghalebizade@gmail.com>
2024-04-21 22:28:57 -0700

    Add Zoom

--- 36 - React.js/45-React-Dependencies-Styling-the-Keeper-App-Starting/src/components/CreateArea.jsx
+++ 36 - React.js/45-React-Dependencies-Styling-the-Keeper-App-Starting/src/components/CreateArea.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
-    content: ""
+    content: "",
   });
 
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setNote(prevNote => {
+    setNote((prevNote) => {
       return {
         ...prevNote,
-        [name]: value
+        [name]: value,
       };
     });
   }
@@ -21,7 +24,7 @@ function CreateArea(props) {
     props.onAdd(note);
     setNote({
       title: "",
-      content: ""
+      content: "",
     });
     event.preventDefault();
   }
@@ -42,7 +45,11 @@ function CreateArea(props) {
           placeholder="Take a note..."
           rows="3"
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={true}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
